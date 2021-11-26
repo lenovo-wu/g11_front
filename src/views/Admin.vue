3619d49fe55aaa0121555e9dc3d640cf09bc3a5b
wlx <31901209@stu.zucc.edu.cn>
2021-11-26 11:44:14 +0800

    管理员界面修改

--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -10,11 +10,51 @@
         <Aside />
 
 <!-- 内容区域 -->
-    <div>
-    </div>
+        <router-view style="flex: 1"/>
+        <el-table
+    :data="tableData"
+    border="true"
+    style="width: 100%; padding: 10px">
+     <el-table-column
+      prop="id"
+      label="用户ID"
+      >
+    </el-table-column>   
+     <el-table-column
+      prop="name"
+      label="用户昵称"
+      >
+    </el-table-column>
+/*用户密码不显示*/      
+    <el-table-column
+      prop="date"
+      label="注册日期"
+      sortable="">
+    </el-table-column>
+    <el-table-column
+      prop="state"
+      label="用户状态"
+      >
+    </el-table-column>
+    <el-table-column
+      prop="sex"
+      label="用户性别">
+    </el-table-column>
+    <el-table-column
+      prop="jurisdiction"
+      label="用户权限">
+    </el-table-column>
+    <el-table-column
+      prop="signature"
+      label="个性签名"
+      show-overflow-tooltip="true">
+    </el-table-column>         
+  </el-table>
     </div>   
 </div>
 </template>
+
+
 
 <script>
 import Header from "@/components/Header";
@@ -25,5 +65,50 @@ export default{
         Header,
         Aside
     },
+    data() {
+      return {
+        tableData: [{
+          id: "31901209",
+          name: "Lenovo",
+          date: '2021-11-09',
+          state: '正常',
+          sex: "男",
+          jurisdiction: "管理员",
+          signature: "",
+        },{
+          id: "31901210",
+          name: "Lenovo",
+          date: '2021-11-10',
+          state: '正常',
+          sex: "男",
+          jurisdiction: "管理员",
+          signature: "",
+        },{
+          id: "31901211",
+          name: "Lenovo",
+          date: '2021-11-11',
+          state: '正常',
+          sex: "男",
+          jurisdiction: "管理员",
+          signature: "",
+        },{
+          id: "31901212",
+          name: "Lenovo",
+          date: '2021-11-12',
+          state: '正常',
+          sex: "男",
+          jurisdiction: "管理员",
+          signature: "",
+        },{
+          id: "31901213",
+          name: "Lenovo",
+          date: '2021-11-13',
+          state: '正常',
+          sex: "男",
+          jurisdiction: "管理员",
+          signature: "",
+        }]
+      }
+    }    
 }
 </script>
