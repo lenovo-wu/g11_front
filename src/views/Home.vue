f619df713837c823bef4c3e6bf61a08bf307e494
g011 <1456034259@qq.com>
2021-11-27 14:37:50 +0800

    0

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,21 @@
 <template>
   <div>
+<<<<<<< HEAD
     <div class="box">🔔{{billboard.userName+",你好！"}}</div>
     <el-button type="primary" class="admin" @click="inadmin">进入管理员界面</el-button>
     
+=======
+    <HomeHeader />
+>>>>>>> 50a64eb777c2d01f332d407c994274ae6a0a4294
   </div>
 </template>
 
 <script>
+
 import{getBillboard} from '@/api/billboard'
+import HomeHeader from '../components/HomeHeader.vue'
 export default {
+  components: { HomeHeader },
   name: 'Home',
   data(){
     return{
