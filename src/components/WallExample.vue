2cc96b31525d64f42ccaf23466aaad1e959509af
ZUCC-zhj <1325060469@qq.com>
2021-11-27 15:34:08 +0800

    123

---
+++ src/components/WallExample.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="wallexample" >
+        
+
+    </div>
+</template>
+
+<style>
+.wallexample{
+width: 950px;
+height: 480px;
+background-color: #FFFFFF;
+position: relative;
+margin: auto;
+}
+</style>
+
+
+
+<script>
+
+export default{
+    name: "WallExample",
+data(){
+    return{
+      billboard:{
+        userName:'wlx'
+      }
+    }
+  },
+  created(){
+    this.fetchBillboard()
+  },
+  methods:{
+    async fetchBillboard(){
+      getBillboard().then((value) => {
+        const { data } = value;
+        this.billboard = data
+      }
+      )
+    },
+    inadmin(){
+      this.$router.push({path:'/Admin'})
+    }
+  }
+}
+</script>
+
+
+
