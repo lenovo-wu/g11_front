8e99da1be52441ad0727d07bbf9566cb949b7153
ZUCC-zhj <1325060469@qq.com>
2021-12-12 19:09:13 +0800

    rank

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -2,19 +2,19 @@
 <template style="height:2000px">
   <div style="background-color:#f5f5f5">
     <HomeHeader />
-    <WallFormal />
+    <WallRank />
     <Footterbg />
   </div>
 </template>
 <script>
 import Footterbg from '../components/footterbg.vue'
 import HomeHeader from '../components/HomeHeader.vue'
-import WallFormal from '../components/WallFormal.vue'
+import WallRank from '../components/WallRank.vue'
 export default {
   components: { 
     HomeHeader,
-    WallFormal,
     Footterbg,
+    WallRank,
      },
   name: 'Rank',
   data(){
