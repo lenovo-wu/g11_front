5200465986bce86f8a3f88e0fe0200e32f8ce591
g011 <1456034259@qq.com>
2021-12-22 10:27:21 +0800

    个人中心背景图片位置

---
+++ src/components/footter2.vue
@@ -0,0 +1,43 @@
+// 首页页脚，放置表白图片，增添色彩
+<template>
+    <div class="footter2">
+        
+
+    </div>
+
+    
+</template>
+
+<script>
+
+
+export default {
+  components: {  },
+    name: "Footter2",
+    data(){
+    return{
+      
+    }
+  },
+
+  methods:{
+}
+}
+</script>
+
+<style >
+.footter2{
+  position: absolute;
+   width: 1200px;
+   height: 840px;
+   background-image: url(../assets/css/rainylove.png);
+   /* background-repeat:repeat-x; */
+   margin-left:-600px;
+   left: 50%;
+   
+   top: 540px;
+   z-index: 10;
+
+}
+
+</style>
